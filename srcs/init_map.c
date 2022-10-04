12d2aefef866e54119ace10044ad02dd6e567c0a
mumontei <mumontei@student.42sp.org.br>
2022-10-04 23:53:45 +0200

    norminette run

--- srcs/init_map.c
+++ srcs/init_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mumontei <mumontei@42.sp.org>              +#+  +:+       +#+        */
+/*   By: mumontei <mumontei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:46:58 by mumontei          #+#    #+#             */
-/*   Updated: 2022/10/04 12:59:25 by mumontei         ###   ########.fr       */
+/*   Updated: 2022/10/04 23:52:37 by mumontei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ void	init_map(t_game *game, char *mapfile)
 
 	map_fd = open(mapfile, O_RDONLY);
 	if (map_fd == -1)
+	{
 		write(1, "Error. Map couldn't be loaded.\n", 31);
 		return ;
+	}
 	map_temp = ft_strdup("");
 	game->map.rows = 0;
 	while (1)
@@ -39,8 +41,8 @@ void	init_map(t_game *game, char *mapfile)
 
 char	*map_concat(char **s1, const char *s2)
 {
-	char *s;
-	
+	char	*s;
+
 	if (!s1 || !s2)
 		return (NULL);
 	s = (char *)ft_calloc(ft_strlen(*s1) + ft_strlen(s2) + 1, sizeof(char));
@@ -49,5 +51,5 @@ char	*map_concat(char **s1, const char *s2)
 	ft_strlcpy(s, *s1, ft_strlen(*s1) + 1);
 	ft_strlcat(s, s2, ft_strlen(*s1) + ft_strlen(s2) + 1);
 	free(*s1);
-	return(s);
+	return (s);
 }
